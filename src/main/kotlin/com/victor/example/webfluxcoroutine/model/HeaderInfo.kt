d55fc1342e1b15d82c3b998f5e9f4dda5bdac8a2
victor.cha <victor.cha@kakaopaycorp.com>
2020-04-14 15:20:45 +0900

    remove unused code

--- src/main/kotlin/com/victor/example/webfluxcoroutine/model/HeaderInfo.kt
+++ src/main/kotlin/com/victor/example/webfluxcoroutine/model/HeaderInfo.kt
@@ -8,17 +8,12 @@ import org.springframework.web.reactive.function.server.ServerRequest
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
 
-data class HeaderInfo(
-        val clientIp: String,
-        val appUuid: String,
-        val userAgent: String) {
+data class HeaderInfo(val userAgent: String) {
     companion object {
         @JvmStatic
         fun resolve(req: ServerRequest): Mono<HeaderInfo> =
                 HeaderInfo(
-                        clientIp = req.remoteIpWithoutMono(),
                         userAgent = req.getHeaderOrEmptyString(HEADER_USER_AGENT),
-                        appUuid = req.appUuidWithoutMono()
                 ).toMono()
 
     }
