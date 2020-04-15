18affa7445fb7f281b0caa055e996d019e6c9cb1
victor.cha <victor.cha@kakaopaycorp.com>
2020-04-15 16:53:17 +0900

    refactoring

--- src/main/kotlin/com/victor/example/webfluxcoroutine/model/HeaderInfo.kt
+++ src/main/kotlin/com/victor/example/webfluxcoroutine/model/HeaderInfo.kt
@@ -1,11 +1,11 @@
 package com.victor.example.webfluxcoroutine.model
 
 import com.victor.example.kotlinx.getHeaderOrEmptyString
-import com.victor.example.webfluxcoroutine.HEADER_USER_AGENT
 import org.springframework.web.reactive.function.server.ServerRequest
 
 data class HeaderInfo(val userAgent: String) {
     companion object {
+        private const val HEADER_USER_AGENT = "User-Agent"
         @JvmStatic
         fun of(req: ServerRequest): HeaderInfo =
                 HeaderInfo(
