0d8f1ffa5b26b2c011faa8104b23246585739088
victor.cha <victor.cha@kakaopaycorp.com>
2020-04-15 22:01:19 +0900

    apply buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,49 +2,23 @@ import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
-val resilience4jVersion = "1.1.0"
-val springKafkaVersion = "2.3.1.RELEASE"
-
-val javaxServletApiVersion = "3.1.0"
-val reactorAddonVersion = "3.2.0.RELEASE"
-val redissonVersion = "3.6.5"
-val modelMapperVersion = "0.7.5"
-
-val fstVersion = "2.55"
-val snappyVersion = "1.1.7.1"
-
-val httpclientVersion = "4.3.4"
-
-val micrometerVersion="1.0.6"
-val logbackVersion="8.0.3"
-
-val groovyVersion="2.4.13"
-val spockVersion="1.1-groovy-2.4"
-val mockitoKotlinVersion="1.5.0"
-val embeddedRedisVersion="0.6"
-val reactorKotlinExVersion="1.0.2.RELEASE"
-
 plugins {
-    val kotlinVersion = "1.3.50"
-    val springbootVersion = "2.2.0.RELEASE"
     idea
     java
     jacoco
     groovy
-    kotlin("plugin.jpa") version kotlinVersion
-    kotlin("jvm") version kotlinVersion
-    kotlin("plugin.spring") version kotlinVersion
-    kotlin("kapt") version kotlinVersion
-    id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
-    id("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
-
-    id("org.springframework.boot") version springbootVersion
-    id("io.spring.dependency-management") version "1.0.7.RELEASE"
-
+    kotlin("plugin.jpa") version Versions.Plugins.kotlin
+    kotlin("jvm") version Versions.Plugins.kotlin
+    kotlin("plugin.spring") version Versions.Plugins.kotlin
+    kotlin("kapt") version Versions.Plugins.kotlin
+    id("org.jetbrains.kotlin.plugin.allopen") version Versions.Plugins.kotlin
+    id("org.jetbrains.kotlin.plugin.noarg") version Versions.Plugins.kotlin
+    id("org.springframework.boot") version Versions.Plugins.springboot
+    id("io.spring.dependency-management") version Versions.Plugins.dependencyManagement
 }
 
-group = "com.victor.example.webfluxcoroutine"
-version = "0.0.1-SNAPSHOT"
+group = "${Release.group}${Release.name}"
+version = Release.version
 
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
@@ -57,7 +31,7 @@ tasks.withType<KotlinCompile> {
 }
 
 tasks.named<BootJar>("bootJar") {
-    archiveBaseName.set("webflux-coroutine")
+    archiveBaseName.set(Release.name)
 }
 
 tasks.named<Jar>("jar") {
@@ -73,52 +47,44 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-devtools")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation(Dependencies.Spring.devTools)
+    implementation(Dependencies.Spring.thymeleaf)
+    implementation(Dependencies.Spring.actuator)
+    implementation(Dependencies.Spring.webflux)
+    implementation(Dependencies.Spring.kafka)
+    implementation(Dependencies.Spring.reactorAddOn)
 
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation(Dependencies.Kotlin.jackson)
+    implementation(Dependencies.Kotlin.stdlibJdk8)
+    implementation(Dependencies.Kotlin.reflect)
+    implementation(Dependencies.Kotlin.coroutineCore)
+    implementation(Dependencies.Kotlin.coroutineReactor)
+    implementation(Dependencies.Kotlin.reactorExtension)
 
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:$reactorKotlinExVersion")
+    implementation(Dependencies.Resilience4j.core)
+    implementation(Dependencies.Resilience4j.reactor)
+    implementation(Dependencies.Resilience4j.micrometer)
 
-    // resilience4j []
-    implementation("io.github.resilience4j:resilience4j-all:$resilience4jVersion")
-    implementation("io.github.resilience4j:resilience4j-reactor:$resilience4jVersion")
-    implementation("io.github.resilience4j:resilience4j-micrometer:$resilience4jVersion")
-    // resilience4j []
+    implementation(Dependencies.micrometerPrometheus)
+    implementation(Dependencies.redisson)
+    implementation(Dependencies.fst)
+    implementation(Dependencies.snappy)
+    implementation(Dependencies.modelMapper)
+    implementation(Dependencies.httpClient)
+    implementation(Dependencies.guava)
+    implementation(Dependencies.commonLang3)
 
-    implementation("io.micrometer:micrometer-registry-prometheus:$micrometerVersion")
+    testImplementation(TestDependencies.Spring.test)
+    testImplementation(TestDependencies.Spring.restdocMockMvc)
+    testImplementation(TestDependencies.reactorTest)
+    testImplementation(TestDependencies.embeddedRedis)
+    testImplementation(TestDependencies.javaxInject)
 
-    implementation("org.springframework.kafka:spring-kafka:$springKafkaVersion")
-
-    implementation("io.projectreactor.addons:reactor-extra:$reactorAddonVersion")
-
-    implementation("org.redisson:redisson:$redissonVersion")
-    implementation("de.ruedigermoeller:fst:$fstVersion")
-    implementation("org.xerial.snappy:snappy-java:$snappyVersion")
-    implementation("com.getsentry.raven:raven-logback:$logbackVersion")
-    implementation("org.modelmapper:modelmapper:$modelMapperVersion")
-    implementation("org.apache.httpcomponents:httpclient:$httpclientVersion")
-    implementation("com.google.guava:guava:23.0")
-    implementation("org.apache.commons:commons-lang3:3.9")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
-    testImplementation("com.github.kstyrc:embedded-redis:$embeddedRedisVersion")
-    testImplementation("javax.inject:javax.inject:1")
-    // mandatory dependencies for using Spock[]
-    testImplementation("org.codehaus.groovy:groovy-all:$groovyVersion")
-    testImplementation("org.spockframework:spock-core:$spockVersion")
+    testImplementation(TestDependencies.Spock.groovy)
+    testImplementation(TestDependencies.Spock.core)
 
     // https://mvnrepository.com/artifact/com.nhaarman/mockito-kotlin
-    testImplementation("com.nhaarman:mockito-kotlin:$mockitoKotlinVersion")
-    // mandatory dependencies for using Spock[]
+    testImplementation(TestDependencies.mockitoKotlin)
 }
 
 configure<DependencyManagementExtension> {
---
+++ buildSrc/src/main/kotlin/Build.kt
@@ -0,0 +1,88 @@
+object Release {
+    const val group = "com.victor.example."
+    const val name = "webfluxcoroutine"
+    const val version = "0.0.1-SNAPSHOT"
+}
+
+object Versions {
+    object Plugins {
+        const val kotlin = "1.3.50"
+        const val springboot = "2.2.0.RELEASE"
+        const val dependencyManagement = "1.0.7.RELEASE"
+    }
+
+    const val resilience4j = "1.1.0"
+    const val springKafka = "2.3.1.RELEASE"
+
+    const val reactorAddon = "3.2.0.RELEASE"
+    const val reactorKotlinEx = "1.0.2.RELEASE"
+    const val redisson = "3.6.5"
+    const val modelMapper = "0.7.5"
+
+    const val guava = "29.0-jre"
+    const val commonsLang3 = "3.9"
+
+    const val fst = "2.55"
+    const val snappy = "1.1.7.1"
+
+    const val httpClient = "4.3.4"
+
+    const val micrometer = "1.0.6"
+
+    const val groovy = "2.4.13"
+    const val spock = "1.1-groovy-2.4"
+    const val mockitoKotlin = "1.5.0"
+    const val embeddedRedis = "0.6"
+}
+
+object TestDependencies {
+    object Spring {
+        const val test = "org.springframework.boot:spring-boot-starter-test"
+        const val restdocMockMvc = "org.springframework.restdocs:spring-restdocs-mockmvc"
+    }
+
+    object Spock {
+        const val groovy = "org.codehaus.groovy:groovy-all:${Versions.groovy}"
+        const val core = "org.spockframework:spock-core:${Versions.spock}"
+    }
+    const val mockitoKotlin = "com.nhaarman:mockito-kotlin:${Versions.mockitoKotlin}"
+    const val reactorTest = "io.projectreactor:reactor-test"
+    const val embeddedRedis = ("com.github.kstyrc:embedded-redis:${Versions.embeddedRedis}")
+    const val javaxInject = "javax.inject:javax.inject:1"
+}
+
+object Dependencies {
+    object Spring {
+        const val devTools = "org.springframework.boot:spring-boot-devtools:${Versions.Plugins.springboot}"
+        const val actuator = "org.springframework.boot:spring-boot-starter-actuator:${Versions.Plugins.springboot}"
+        const val webflux = "org.springframework.boot:spring-boot-starter-webflux:${Versions.Plugins.springboot}"
+        const val kafka = "org.springframework.kafka:spring-kafka:${Versions.springKafka}"
+        const val thymeleaf = "org.springframework.boot:spring-boot-starter-thymeleaf:${Versions.Plugins.springboot}"
+        const val reactorAddOn = "io.projectreactor.addons:reactor-extra:${Versions.reactorAddon}"
+    }
+
+    object Kotlin {
+        const val jackson = "com.fasterxml.jackson.module:jackson-module-kotlin"
+        const val stdlibJdk8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
+        const val reflect = "org.jetbrains.kotlin:kotlin-reflect"
+        const val coroutineCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core"
+        const val coroutineReactor = "org.jetbrains.kotlinx:kotlinx-coroutines-reactor"
+        const val reactorExtension = "io.projectreactor.kotlin:reactor-kotlin-extensions:${Versions.reactorKotlinEx}"
+    }
+
+    object Resilience4j {
+        const val core = "io.github.resilience4j:resilience4j-all:${Versions.resilience4j}"
+        const val reactor = "io.github.resilience4j:resilience4j-reactor:${Versions.resilience4j}"
+        const val micrometer = "io.github.resilience4j:resilience4j-micrometer:${Versions.resilience4j}"
+    }
+
+
+    const val micrometerPrometheus = "io.micrometer:micrometer-registry-prometheus:${Versions.micrometer}"
+    const val redisson = "org.redisson:redisson:${Versions.redisson}"
+    const val fst = "de.ruedigermoeller:fst:${Versions.fst}"
+    const val snappy = "org.xerial.snappy:snappy-java:${Versions.snappy}"
+    const val modelMapper = "org.modelmapper:modelmapper:${Versions.modelMapper}"
+    const val httpClient = "org.apache.httpcomponents:httpclient:${Versions.httpClient}"
+    const val guava = "com.google.guava:guava:${Versions.guava}"
+    const val commonLang3 = "org.apache.commons:commons-lang3:${Versions.commonsLang3}"
+}
